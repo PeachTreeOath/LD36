8fc2932911e2113cae16ed46ff45bc355ce4dc76
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 12:26:13 -0600

    Spawn test building

--- Assets/Resources/Scripts/BuildingStats.cs
+++ Assets/Resources/Scripts/BuildingStats.cs
@@ -7,6 +7,14 @@ public class BuildingStats : MonoBehaviour
     public float maxHealth;
     public float curHealth;
     public int oilValue;
+    public float startingSpawnNum;
+    public float spawnRadius;
+    public float spawnRate;
+
+    /// <summary>
+    /// Only start spawning when player is near so it doesn't get overwhelming
+    /// </summary>
+    public const float BEGIN_SPAWN_RADIUS = 5f;
 
     void Start()
     {
