c78c45517c54827ad30c5d6236c0b572ff684c0c
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 02:28:58 -0600

    Building explosion

--- Assets/Resources/Scripts/Building.cs
+++ Assets/Resources/Scripts/Building.cs
@@ -7,12 +7,15 @@ public class Building : MonoBehaviour
     public BuildingStats stats;
 
     private GameObject barrelObj;
+    private GameObject fireObj;
     private Sprite rubbleSpr;
+    private bool isAlive = true;
 
     // Use this for initialization
     void Start()
     {
         barrelObj = Resources.Load<GameObject>("Prefabs/OilBarrel");
+        fireObj = Resources.Load<GameObject>("Prefabs/FireParticle");
         rubbleSpr = Resources.Load<Sprite>("Images/Gas_Station_Destroy");
     }
 
@@ -24,15 +27,21 @@ public class Building : MonoBehaviour
 
     public void TakeDamage(int dmg)
     {
+        if (!isAlive)
+        {
+            return;
+        }
         stats.curHealth -= dmg;
         if (stats.curHealth <= 0)
         {
             SpawnBarrel();
+            isAlive = false;
         }
     }
 
     private void SpawnBarrel()
     {
+        Instantiate(fireObj, transform.position, fireObj.transform.rotation);
         OilBarrel barrel = ((GameObject)Instantiate(barrelObj, transform.position, Quaternion.identity)).GetComponent<OilBarrel>();
         barrel.SetValue(stats.oilValue);
         GetComponent<SpriteRenderer>().sprite = rubbleSpr;
