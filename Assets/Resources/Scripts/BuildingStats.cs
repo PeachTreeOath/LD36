34f2d27b7129854dc49956f246553126bee30991
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 12:42:43 -0600

    Spawn manager

--- Assets/Resources/Scripts/BuildingStats.cs
+++ Assets/Resources/Scripts/BuildingStats.cs
@@ -11,10 +11,7 @@ public class BuildingStats : MonoBehaviour
     public float spawnRadius;
     public float spawnRate;
 
-    /// <summary>
-    /// Only start spawning when player is near so it doesn't get overwhelming
-    /// </summary>
-    public const float BEGIN_SPAWN_RADIUS = 5f;
+    public float BEGIN_SPAWN_RADIUS = 5f;
 
     void Start()
     {
---
+++ Assets/Resources/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+    public static SpawnManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
