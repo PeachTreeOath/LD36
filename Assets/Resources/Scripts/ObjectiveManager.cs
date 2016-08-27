0f01e334cf2e5e7132460c76547b183b7b84bbcd
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 15:25:07 -0600

    Objectives

--- Assets/Resources/Scripts/ObjectiveManager.cs
+++ Assets/Resources/Scripts/ObjectiveManager.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class ObjectiveManager : MonoBehaviour
 {
     public static ObjectiveManager instance;
 
     public List<Building> objectives;
+    private int currentObjective;
 
     void Awake()
     {
@@ -31,6 +33,29 @@ public class ObjectiveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (objectives.Count > 0)
+        {
+            // Place arrow if on screen
+            if (CheckIfOnScreen(objectives[currentObjective].gameObject))
+            {
 
+            }
+        }
+    }
+
+    private bool CheckIfOnScreen(GameObject obj)
+    {
+        Vector2 point = Camera.main.WorldToViewportPoint(obj.transform.position);
+
+        if (point.x > 0 && point.y > 0 && point.x < 1 && point.y < 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void RegisterObjective(Objective objective)
+    {
+        objectives.Insert(objective.objectiveNum, objective.GetComponent<Building>());
     }
 }
