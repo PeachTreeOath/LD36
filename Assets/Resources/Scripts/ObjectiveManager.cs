bb8852369f96a8895b1609eb84e1801b162a5d12
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 16:00:37 -0600

    Arrow objectives

--- Assets/Resources/Scripts/ObjectiveManager.cs
+++ Assets/Resources/Scripts/ObjectiveManager.cs
@@ -8,7 +8,9 @@ public class ObjectiveManager : MonoBehaviour
     public static ObjectiveManager instance;
 
     public List<Building> objectives;
+
     private int currentObjective;
+    private GameObject arrow;
 
     void Awake()
     {
@@ -27,7 +29,7 @@ public class ObjectiveManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        arrow = Instantiate(Resources.Load<GameObject>("Prefabs/Arrow"));
     }
 
     // Update is called once per frame
@@ -35,10 +37,12 @@ public class ObjectiveManager : MonoBehaviour
     {
         if (objectives.Count > 0)
         {
+            GameObject currObj = objectives[currentObjective].gameObject;
             // Place arrow if on screen
-            if (CheckIfOnScreen(objectives[currentObjective].gameObject))
+            if (CheckIfOnScreen(currObj))
             {
-
+                arrow.transform.position = currObj.transform.position + new Vector3(0, 2f,0);
+                arrow.transform.LookAt(currObj.transform);
             }
         }
     }
