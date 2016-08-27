8df4d5dbecda7c6ac40a3213584c20ee01c95dab
Dave Flanagan <dflanagan314@gmail.com>
2016-08-27 00:33:25 -0400

    Building stats stub

---
+++ Assets/Resources/Scripts/BuildingStats.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingStats : MonoBehaviour {
+
+    protected float maxHealth { get; set; }
+    protected float curHealth { get; set; }
+
+
+}
