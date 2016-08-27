9bf45b342bd68cfa0aeaf89dba74141dfc7d352f
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-26 22:36:18 -0600

    Dino movement in scene

--- Assets/Resources/Scripts/DinoController.cs
+++ Assets/Resources/Scripts/DinoController.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DinoController : MonoBehaviour {
+public class DinoController : MonoBehaviour
+{
     public float minMouseDist;
+    public float playerSpeed = 0.1f;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float dist = Vector2.Distance(transform.position, mousePos);
-        
-	}
+        if (dist > minMouseDist)
+        {
+            Vector3 direction = Vector3.Normalize(mousePos - (Vector2)transform.position);
+            transform.Translate(direction * playerSpeed);
+        }
+    }
 }
