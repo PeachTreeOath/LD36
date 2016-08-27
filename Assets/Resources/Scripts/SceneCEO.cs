e5758b5fd5f2b65d068c6e57601ea086c43f69f6
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 14:04:17 -0600

    Adding Trump

---
+++ Assets/Resources/Scripts/SceneCEO.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Used to we can stop touching scene everytime we need a new manager
+/// </summary>
+public class SceneCEO : MonoBehaviour
+{
+
+    public GameObject[] managerList;
+
+    void Awake()
+    {
+        foreach (GameObject manager in managerList)
+        {
+            GameObject mgr = Instantiate(manager);
+            mgr.transform.SetParent(transform);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
