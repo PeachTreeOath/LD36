059a08d9f3c05bf17501f03058dfc2ac5ffca1a8
Fangor <rfonger@gmail.com>
2016-08-27 19:56:58 -0600

    Adding proper scripts to new gas statiosn

--- Assets/Resources/Scripts/AttackBuilding.cs
+++ Assets/Resources/Scripts/AttackBuilding.cs
@@ -3,24 +3,31 @@ using System.Collections;
 
 public class AttackBuilding : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private float timeOfLastAttack;
+    private GameObject statsPrefab;
+    
+    public BuildingStats stats; 
+
+    void Start () {
+        statsPrefab = Resources.Load<GameObject>("Prefabs/AttackBuildingStats");
+        stats = ((GameObject)Instantiate(statsPrefab)).GetComponent<BuildingStats>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
         Debug.Log("Something should be colliding with this building.");
         FriendlyAgent minion = col.gameObject.GetComponent<FriendlyAgent>();
-        if (minion != null)
+        if (minion != null && Time.time > timeOfLastAttack + stats.secondsPerAttack)
         {
+
             Debug.Log("Minion taking damage from building.");
-            minion.TakeDamage(5);
+            minion.TakeDamage(stats.attackDamage);
         }
     }
 }
