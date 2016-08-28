d446f7913548e6b4824a31abd4b3f11b8ff6a2db
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-27 22:33:45 -0600

    bg mgr temp fail

--- Assets/Resources/Scripts/BackgroundManager.cs
+++ Assets/Resources/Scripts/BackgroundManager.cs
@@ -23,16 +23,20 @@ public class BackgroundManager : MonoBehaviour
         GameObject bgPrefab = Resources.Load<GameObject>("Prefabs/BGTile");
         Vector2 size = bgPrefab.GetComponent<SpriteRenderer>().bounds.size;
 
-        float startX = 2 / 2 * -1.5f;
-        float startY = 2 / 2 * -1.5f;
+        float startX = size.x * -.5f;
+        float startY = size.y * .5f;
         float currX = startX;
         float currY = startY;
+
         for (int i = 0; i < rowNum; i++)
         {
             for (int j = 0; j < colNum; j++)
             {
                 GameObject bg = ((GameObject)Instantiate(bgPrefab, Vector2.zero, Quaternion.identity));
+                bg.transform.position = new Vector2(currX, currY);
+                currY += size.y;
             }
+            currX += size.x;
         }
     }
 }
