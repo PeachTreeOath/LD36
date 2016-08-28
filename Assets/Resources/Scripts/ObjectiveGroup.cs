28dbcf355d692e80237b1701e9de5edb76382ecb
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-28 17:54:02 -0600

    Objective groups

---
+++ Assets/Resources/Scripts/ObjectiveGroup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectiveGroup {
+
+    public List<Objective> objectives;
+
+    public ObjectiveGroup()
+    {
+        objectives = new List<Objective>();
+    }
+}
