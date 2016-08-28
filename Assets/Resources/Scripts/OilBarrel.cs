66b14e8bebe6b13577ff649e25696a12d5b9d3a0
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-28 00:05:30 -0600

    Barrel spawn anim

--- Assets/Resources/Scripts/OilBarrel.cs
+++ Assets/Resources/Scripts/OilBarrel.cs
@@ -5,17 +5,30 @@ public class OilBarrel : MonoBehaviour
 {
 
     private int value;
+    private bool isDeploying = true;
+    private float elapsedTime;
+    private Vector2 origPos;
 
     // Use this for initialization
     void Start()
     {
-
+        elapsedTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (isDeploying == true)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = new Vector2(origPos.x, origPos.y + Mathf.Sin(elapsedTime * 3));
+            if(transform.position.y < origPos.y)
+            {
+                transform.position = origPos;
+                isDeploying = false;
+                GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -26,11 +39,11 @@ public class OilBarrel : MonoBehaviour
             player.AddOil(value);
             Destroy(gameObject);
         }
-
     }
 
     public void SetValue(int val)
     {
+        origPos = transform.position;
         value = val;
         //float sizeMult = 1 + value / 10;
         float sizeMult = 1;
